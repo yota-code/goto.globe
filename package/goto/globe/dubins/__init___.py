2e9645f8c3538c4b45d641441e399696b085cfb6
yoochan <yota.news@gmail.com>
2022-12-11 21:38:44 +0100

    eow

--- package/goto/globe/dubins/__init___.py
+++ package/goto/globe/dubins/__init___.py
@@ -7,17 +7,19 @@ import goto.globe
 
 from goto.globe.plot import GlobePlotMpl
 
-class Dubincz_CLC() :
-	def __init__(self, Az: g3d.Vector, Ay: g3d.Vector, Ar: float, Aw: int, Bz: g3d.Vector, By: g3d.Vector, Br: float, Bw: int) :
+class Dubincc_CLC() :
+	def __init__(self, Az: g3d.Vector, Ax: g3d.Vector, Ar: float, Aw: int, Bz: g3d.Vector, Bx: g3d.Vector, Br: float, Bw: int) :
 		self.Az = Az # center of the initial circle
-		self.Ay = Ay # element of the frame which points toward the entry point
+		self.Ax = Ax # element of the frame which points toward the entry point
 		self.Ar = Ar # radius of the initial circle (in radians)
 		self.Aw = Aw # 1 if turn right, -1 if turn left
+		self.Ay = -self.Aw * (self.Az @ self.Ax).normalized()
 
 		self.Bz = Bz # center of the final circle
-		self.By = By # element of the frame which points toward the exit point
+		self.Bx = Bx # element of the frame which points toward the exit point
 		self.Br = Br # radius of the final circle (in radians)
 		self.Bw = Bw # 1 if turn right, -1 if turn left
+		self.By = -self.Bw * (self.Bz @ self.Bx).normalized()
 
 		self.AB = self.Az.angle_to(self.Bz)
 
@@ -26,54 +28,96 @@ class Dubincz_CLC() :
 		self.Cx = self.Az
 		self.Cy = self.Cz @ self.Cx
 
-		print(self.Ar, self.Br, self.AB)
+		self.compute_points()
+		self.distance = self.compute_distance()
 
-	def resolve(self) :
+	def compute_points(self) :
 		if self.Bw * self.Aw > 0 :
-			if self.AB < math.pi / 2 :
-				theta = self.Aw * self.theta_direct()
-				psi = self.Aw *  math.asin(math.sin(self.Ar) / math.sin(theta)) + math.pi / 2
+			u = 1 if self.AB < math.pi / 4 else -1
+			theta = self.Aw * self.theta_direct()
+			psi = u * (self.Aw *  math.asin(math.sin(self.Ar) / math.sin(theta)) + math.pi / 2)
 		else :
+			print("CAS 2")
 			theta = self.Aw * self.theta_crossed()
 			psi = self.Aw *  math.asin(math.sin(self.Ar) / math.sin(theta)) + math.pi / 2
 
 		Dy = self.Cx.deflect(self.Cy, psi)
 		Dz = self.Cz.deflect(Dy, theta)
 
-		E = self.Az.project_normal(Dz).normalized()
-		F = self.Bz.project_normal(Dz).normalized()
+		self.E = self.Az.project_normal(Dz).normalized() # end of the first circle, start of the line
+		self.F = self.Bz.project_normal(Dz).normalized() # start of the second circle, end of the line
 
-		I = self.Az.deflect(self.Ay, self.Ar)
-		O = self.Bz.deflect(self.By, self.Br)
+		assert(abs(self.Az.angle_to(self.E) - self.Ar) < 0.25 / goto.globe.earth_radius)
+		assert(abs(self.Bz.angle_to(self.F) - self.Br) < 0.25 / goto.globe.earth_radius)
 
-		print(math.degrees(theta), math.degrees(psi))
-		print(self.Az.angle_to(E) - self.Ar)
-		print(self.Bz.angle_to(F) - self.Br)
+	def compute_distance(self) :
 
-		with GlobePlotMpl() as gpl :
-			#gpl.add_point(self.Az, 'Az', 'r')
-			#gpl.add_point(self.Ay, 'Ay', 'r')
-			gpl.add_circle(self.Az, self.Ar, 'r')
-			#gpl.add_point(self.Bz, 'Bz', 'b')
-			#gpl.add_point(self.By, 'By', 'b')
-			gpl.add_circle(self.Bz, self.Br, 'b')
-			gpl.add_line(self.Az, self.Bz, 'g')
-			gpl.add_great_circle(Dz, 'magenta')
+		self.e = ( self.E.project_normal(self.Az).atan2(self.Ay, self.Ax)) % math.tau
+		self.f = (-self.F.project_normal(self.Bz).atan2(self.By, self.Bx)) % math.tau
+		self.d = self.E.angle_to(self.F)
 
-			gpl.add_point(self.Cx, 'Cx', 'cyan')
-			gpl.add_point(self.Cy, 'Cy', 'cyan')
-			gpl.add_point(self.Cz, 'Cz', 'cyan')
+		return math.sin(self.Ar) * self.e + self.d + math.cos(self.Br) * self.f
 
-			#gpl.add_point(Dx, 'Dx', 'orange')
-			gpl.add_point(Dy, 'Dy', 'orange')
-			gpl.add_point(Dz, 'Dz', 'orange')
+	def plot(self) :
+
+		I = self.Az.deflect(self.Ax, self.Ar)
+		O = self.Bz.deflect(self.Bx, self.Br)
+
+		with GlobePlotMpl(f"distance = {self.distance:.3g}", pth=f"save_{'L' if self.Aw < 0 else 'R'}S{'L' if self.Bw < 0 else 'R'}.png") as gpl :
+			# gpl.add_point(self.Ax, 'Ax', 'r')
+			# gpl.add_point(self.Ay, 'Ay', 'g')
+			gpl.add_point(self.Az, 'Az', 'b')
+
+			gpl.add_point(self.Bx, 'Bx', 'r')
+			gpl.add_point(self.By, 'By', 'g')
+			gpl.add_point(self.Bz, 'Bz', 'b')
+
+			gpl.add_circle(self.Az, self.Ar, 'cyan')
+			# #gpl.add_point(self.Bz, 'Bz', 'b')
+			# #gpl.add_point(self.Bx, 'Bx', 'b')
+			gpl.add_circle(self.Bz, self.Br, 'cyan')
+			# gpl.add_line(self.Az, self.Bz, 'g')
+			# gpl.add_great_circle(Dz, 'magenta')
+
+			# gpl.add_point(self.Cx, 'Cx', 'cyan')
+			# gpl.add_point(self.Cy, 'Cy', 'cyan')
+			# gpl.add_point(self.Cz, 'Cz', 'cyan')
+
+			# #gpl.add_point(Dx, 'Dx', 'orange')
+			# gpl.add_point(Dy, 'Dy', 'orange')
+			# gpl.add_point(Dz, 'Dz', 'orange')
 
 			gpl.add_point(I, 'I', 'purple')
 			gpl.add_point(O, 'O', 'purple')
+			gpl.add_point(self.F, 'F', 'k')
 
-			gpl.add_signed_arc(I, E, self.Az, self.Aw)
-			gpl.add_line(E, F)
-			gpl.add_signed_arc(F, O, self.Bz, self.Bw)
+			gpl.add_signed_arc(I, self.E, self.Az, self.Aw)
+			gpl.add_line(self.E, self.F)
+			gpl.add_signed_arc(self.F, O, self.Bz, self.Bw)
+
+	# def plot(self) :
+
+	# 	I = self.Az.deflect(self.Ax, self.Ar)
+	# 	O = self.Bz.deflect(self.Bx, self.Br)
+
+	# 	w = -1
+	# 	Ax, Bx, Cx = self.F, O, self.Bz
+
+	# 	Az = w * (Ax @ Cx).normalized()
+	# 	Ay = w * (Cx @ Az)
+
+	# 	Bz = w * (Bx @ Cx).normalized()
+	# 	By = w * (Cx @ Bz)
+
+
+	# 	with GlobePlotMpl(f"distance = {self.distance:.3g}") as gpl :
+	# 		gpl.add_circle(Cx, self.Br, 'magenta')
+	# 		gpl.add_point(Cx, 'Cx', 'orange')
+	# 		gpl.add_point(Ax, 'Ax', 'r')
+	# 		gpl.add_point(Ay, 'Ay', 'g')
+	# 		gpl.add_point(Az, 'Az', 'b')
+	# 		gpl.add_point(O, 'O', 'k')
+			
 
 
 	def theta_direct(self) :
@@ -97,26 +141,41 @@ class Dubincz_CLC() :
 		)
 
 
-class Dubincz() :
-	def __init__(self, A:g3d.Vector, Ahdg:float, Arad: float, B:g3d.Vector) :
-		self.A = A
-		self.A
+def Dubincc(I:goto.globe.Blip, Ah:float, Av: float, O:goto.globe.Blip, Bh: float, Bv: float) :
+
+	phi_max = 24.0
+
+	Ar = Av**2 / ( goto.globe.earth_gravity * math.tan(math.radians(phi_max)) ) # TODO: on devrait calculer le véritable rayon géodésique du virage (cf scade)
+	Br = Av**2 / ( goto.globe.earth_gravity * math.tan(math.radians(phi_max)) )
+		
+	for Aw in [-1, 1] :
+		for Bw in [-1, 1] :
+			Iz = I.as_vector
+			Ix, Iy = Iz.oriented_frame(math.radians(Ah), Aw)
+			Az = Iz.deflect(Iy, Ar)
+			
+			Oz = O.as_vector
+			Ox, Oy = Oz.oriented_frame(math.radians(Bh), Bw)
+			Bz = Oz.deflect(Oy, Br)
+
 
 
 if __name__ == '__main__' :
 	import goto.globe
 
-	Az = goto.globe.Blip(0.0, -10.0).as_vector
+	Az = goto.globe.Blip(-5.0, -10.0).as_vector
 	Ah = math.radians(45.0)
 	Ar = 2000000 / goto.globe.earth_radius
-	Aw = 1 # turn to the right
-	Ax, Ay = Az.oriented_frame(Ah, -Aw)
+	# Aw = -1 # turn to the right
 
-	Bz = goto.globe.Blip(20.0, 20.0).as_vector
+	Bz = goto.globe.Blip(10.0, 50.0).as_vector
 	Bh = math.radians(60.0)
 	Br = 1500000 / goto.globe.earth_radius
-	Bw = -1 # turn to the right
-	Bx, By = Bz.oriented_frame(Bh, -Bw)
+	# Bw = -1 # turn to the right
 
-	Dubincz_CLC(Az, Ay, Ar, Aw, Bz, By, Br, Bw).resolve()
+	for Aw in [-1, 1] :
+		for Bw in [-1, 1] :
+			Ax, Ax = Az.oriented_frame(Ah, -Aw)
+			Bx, Bx = Bz.oriented_frame(Bh, -Bw)
+			Dubincc_CLC(Az, Ax, Ar, Aw, Bz, Bx, Br, Bw).plot()
 
--- package/goto/globe/plot/mpl.py
+++ package/goto/globe/plot/mpl.py
@@ -27,7 +27,8 @@ class arrow_3d(matplotlib.patches.FancyArrowPatch):
 		return np.min(zs)
 
 class GlobePlotMpl(GlobePlot__base__) :
-	def __init__(self, pth=None) :
+	def __init__(self, title='', pth=None) :
+		self.title = title
 		self.pth = pth
 
 	def __enter__(self) :
@@ -43,6 +44,8 @@ class GlobePlotMpl(GlobePlot__base__) :
 			self.fig = plt.figure()
 		else :
 			self.fig = plt.figure(figsize=(16, 16))
+
+		plt.title(self.title)
 
 		self.axe = self.fig.add_subplot(1, 1, 1, projection='3d')
 		self.axe.plot_surface(sphere_x, sphere_y, sphere_z, color='b', alpha=0.05)
